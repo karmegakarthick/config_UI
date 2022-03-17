4fecd7355b943822a8931462aa73018cc943ab5d
Hemanth Kumar <hemanth.kumar-external@netobjex.com>
2022-03-17 15:34:41 +0530

    configure string changes

--- src/components/Client/conf.js
+++ src/components/Client/conf.js
@@ -1,24 +1,20 @@
-
-module.exports = {value : `module.exports = {
-    globeIdentification: `+{}+`,
-    globeDeployment: "",
-    name: "GlobeChek_DESKTOP",
-    version: "V3B",
-    environment: "production",
-    login: "api",
-    ftpuser: "ftpuser",
-    ftppsw: "GlobeChekPass1!",
-    ftphost: "caecvm.eastus2.cloudapp.azure.com",
-    urlPrereg:
-      "https://globecheckportal-prod-api.azurewebsites.net/api/v1/Desktop/~/",
-    urlPortal: "https://gcdevstor.z20.web.core.windows.net/",
-    urlAPI:
-      "https://globecheckportal-prod-api.azurewebsites.net/api/v1/Desktop/~/",
-    urlCompanyWeb: "https://www.globechek.com/",
-    va: false,
-    maxHeight: "73",
-  };`
-}
-
+import details from "../../components/Client/ClientConfig"
+export default class datamodel {
+  globeIdentification =  "" ;
+  globeDeployment= "" ;
+  name = "GlobeChek_DESKTOP";
+  version = "V3B";
+  environment = "production";
+  login = "api";
+  ftpuser = "ftpuser";
+  ftppsw = "GlobeChekPass1!";
+  ftphost = "caecvm.eastus2.cloudapp.azure.com";
+  urlPrereg = "";
+  urlPortal = "https://gcdevstor.z20.web.core.windows.net/";
+  urlAPI = ""
+  urlCompanyWeb = "https://www.globechek.com/";
+  va= false;
+  maxHeight= "73";
+};
 
   
