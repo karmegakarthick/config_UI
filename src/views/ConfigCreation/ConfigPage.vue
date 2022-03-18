54aff3327bc99f99a6afd47720289c7c95386166
Hemanth Kumar <hemanth.kumar-external@netobjex.com>
2022-03-18 18:11:53 +0530

    changed

--- src/views/ConfigCreation/ConfigPage.vue
+++ src/views/ConfigCreation/ConfigPage.vue
@@ -9,19 +9,27 @@
           <div>
           <input type="text" id="text" v-model="client" placeholder="Organization Name"> <br/>
            <br/>
-          <button id='button' v-on:click="getDetails()">Submit</button>
+          <button id='button-21' v-on:click="getDetails()">Submit</button>
      </div>
 
- <div v-if="IsVisible">
-         <label>LocationName</label>
+
+ <div>
+         <label>ClientName</label>
          <br/>
-         <select id='dropdown' v-model="OrgLocation" name='Select Location'>
-             <option value=""> Select Location </option>
-             <option v-for="item in info"  :value='item.locationName' :key='item.deviceID' >{{item.locationName}} </option>
+         <select id='dropdown' v-model="ClientName" name='Select Location'>
+             <option value=""> ClientName </option>
+             <option v-for="item in info"  :value='item.name' :key='item.deviceID' >{{item.name}} </option>
          </select>
      </div>
-
-      <div v-if="IsVisible"  >
+ <div>
+         <label>LocationName</label>
+         <br/>
+         <select id='dropdown' v-model="ClientName" name='Select Location'>
+             <option value=""> Select Location </option>
+             <option v-for="item in info"  :value='item.name' :key='item.deviceID' >{{item.name}} </option>
+         </select>
+     </div>
+      <div >
          <label>DeviceName</label>
          <br/>
          <select id='dropdown' v-model="devName">
@@ -29,9 +37,10 @@
          </select>
      </div>
 
-     <div v-if="IsVisible">
-          <button id='button' v-on:click="Download()">Download Config File</button>
+     <div >
+          <button id='button-21' v-on:click="Download()">Download Config File</button>
      </div>
+     
 </div>
 
 
@@ -41,13 +50,12 @@
 
 <script>
 import axios from 'axios'
-import datamodel from './conf.js'
 export default {
     name:"ClientConfig",
 data:function() {
     return {
-
-      client:'',
+      Client: '',
+      ClientName:'',
       details:[],
       info:{},
       infodetails:{},
@@ -58,11 +66,22 @@ data:function() {
     }
 }, 
 
+created(){
+          axios.get('http://localhost:6060/api/Home/getClientName').then((res)=>{
+            console.log("response is :",res)
+           this.info=res.data;
+           console.log("clientName",this.info)
+
+        }
+        )
+
+},
+
  methods : {
     getDetails:function(){
         axios.get('http://localhost:6060/api/Home/getGlobeDetails',{
             params:{
-                client: this.client,
+                client: this.ClientName,
             }
         }).then((res)=>{
             console.log("response is :",res)
@@ -76,13 +95,11 @@ data:function() {
         IsVisible:true;
         }
 
+
     },
     Download:function(){
         this.details=this.infodetails.filter(x=> x.deviceName== this.devName);
         console.log("this is the sample",this.details);
-        
-        // var m = module.globeDeployment
-        // m = this.details
 
 var data = `module.exports = {
     globeIdentification:"`+this.details[0].deviceName+`",
@@ -106,11 +123,6 @@ var data = `module.exports = {
 
 
       var all = new datamodel();
-      // all.globeIdentification = this.details[0].deviceName;
-      // all.globeDeployment = this.details[0].clientName+" "+this.details[0].locationName;
-      // all.urlPrereg = "https://globecheckportal-prod-api.azurewebsites.net/api/v1/Desktop/"+this.details[0].clientID+'~'+this.details[0].deviceID+"/";
-      // all.urlAPI = "https://globecheckportal-prod-api.azurewebsites.net/api/v1/Desktop/"+this.details[0].clientID+`~`+this.details[0].deviceID+"/";
-
       var file = new File([ data  ], { "type" : "" });
       let link = document.createElement('a')
       link.href = window.URL.createObjectURL(file)
@@ -125,7 +137,12 @@ var data = `module.exports = {
 watch:{
     OrgLocation:function(){
         return this.infodetails=this.info.filter(x=> x.locationName== this.OrgLocation);
+
     },
+
+    ClientName:function(){
+      getDetails();
+    }
 
 },  
 
@@ -148,8 +165,51 @@ watch:{
   text-decoration: none;
   display: inline-block;
   font-size: 16px;
-  
 }
+#button-21 {
+  align-items: center;
+  appearance: none;
+  background-color: #3EB2FD;
+  background-image: linear-gradient(1deg, #4F58FD, #149BF3 99%);
+  background-size: calc(100% + 20px) calc(100% + 20px);
+  border-radius: 100px;
+  border-width: 0;
+  box-shadow: none;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  cursor: pointer;
+  display: inline-flex;
+  font-family: CircularStd,sans-serif;
+  font-size: 1rem;
+  height: auto;
+  justify-content: center;
+  line-height: 1.5;
+  padding: 6px 20px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  transition: background-color .2s,background-position .2s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: top;
+  white-space: nowrap;
+}
+
+.button-21:active,
+.button-21:focus {
+  outline: none;
+}
+
+.button-21:hover {
+  background-position: -20px -20px;
+}
+
+.button-21:focus:not(:active) {
+  box-shadow: rgba(40, 170, 255, 0.25) 0 0 0 .125em;
+}
+  
+
 #text {
     width: 20%;
   padding: 12px 20px;
@@ -165,5 +225,49 @@ div {
   padding: 5px;
 }
 
+
+</style>
+
+
+
+<style >
+
+
+#button-24 {
+  background-color: #ffffff;
+  background-image: linear-gradient(1deg, #3EB2FD, #ffffff 99%);
+  border: 1px solid #3EB2FD;
+  border-radius: 6px;
+  box-shadow: rgba(0, 0, 0, 0.1) 1px 2px 4px;
+  box-sizing: border-box;
+  color: #FFFFFF;
+  cursor: pointer;
+  display: inline-block;
+  font-family: nunito,roboto,proxima-nova,"proxima nova",sans-serif;
+  font-size: 16px;
+  font-weight: 800;
+  line-height: 16px;
+  min-height: 40px;
+  outline: 0;
+  padding: 12px 14px;
+  text-align: center;
+  text-rendering: geometricprecision; 
+  text-transform: none;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: middle;
+}
+
+#button-24:hover,
+#button-24:active {
+  background-color: initial;
+  background-position: 0 0;
+  color: #3EB2FD;
+}
+
+#button-24:active {
+  opacity: .5;
+}
 
 </style>
---
+++ src/views/ConfigCreation/ConfigRoute.js
@@ -0,0 +1,8 @@
+import ClientConfig from "../ConfigCreation/ConfigPage.vue"
+export const ConfigRoute = [
+
+    {
+        path : "/Config_creation",
+        component : ClientConfig,
+    },
+]
